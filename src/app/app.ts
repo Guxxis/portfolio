7f2ca23b2686c4c1e800493afd479db7ab2ef324
Gustavo Gonçalves <gustavo.goncalves@doutoresdaweb.com.br>
2025-12-04 22:48:15 -0300

    fix: fixed menu position

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,7 +12,7 @@ import { Header } from './components/header/header';
   imports: [ 
     Header, 
     // Footer, 
-    // Banner, 
+    Banner, 
     // Contact, 
     // About, 
     // Projects, 
