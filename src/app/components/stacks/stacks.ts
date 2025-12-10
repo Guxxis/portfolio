940b8207626d9198d2681f1eb27374f06b70e965
Gustavo Gonçalves <gustavo.goncalves@doutoresdaweb.com.br>
2025-12-10 00:02:57 -0300

    refactor: for loop to stacks

--- src/app/components/stacks/stacks.ts
+++ src/app/components/stacks/stacks.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Stacks {
 
+  stacksList: any[] = [
+    { nome: 'Angular', img: 'icons/angular.svg' },
+    { nome: 'Java', img: 'icons/java.svg' },
+    { nome: 'HTML', img: 'icons/html.svg' },
+    { nome: 'CSS', img: 'icons/css.svg' },
+    { nome: 'JS', img: 'icons/javascript.svg' },
+    { nome: 'TypeScript', img: 'icons/typescript.svg' },
+    { nome: 'Tailwind', img: 'icons/tailwind.svg' },
+    { nome: 'AWS', img: 'icons/aws.svg' },
+    { nome: 'Docker', img: 'icons/docker.svg' },
+    { nome: 'Kubernetes', img: 'icons/kubernetes.svg' }
+  ];
 }
