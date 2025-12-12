46661c45e1dfb3ab83cd1e6ff0c44388b8678336
Gustavo Gonçalves <gustavo.goncalves@doutoresdaweb.com.br>
2025-12-12 01:15:59 -0300

    feat: projetos

--- src/app/components/card-project/card-project.ts
+++ src/app/components/card-project/card-project.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export interface Stack {
+    nome: string,
+    img: string,
+}
 
 @Component({
   selector: 'app-card-project',
@@ -6,6 +11,13 @@ import { Component } from '@angular/core';
   templateUrl: './card-project.html',
   styleUrl: './card-project.css',
 })
+
 export class CardProject {
+
+  @Input() textTitle!: string;
+  @Input() textDesc!: string;
+  @Input() stackList: Stack[] = [];
+  @Input() urlPath!: string;
+  @Input() imgPath!: string;
 
 }
